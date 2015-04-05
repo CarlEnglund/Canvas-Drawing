a236c5f55f7b22223c4b8df7059a282852505b0d
CarlEnglund <englund92@gmail.com>
2015-04-05 19:39:20 +0200

    Added files for gh-pages

---
+++ README.md
@@ -0,0 +1,9 @@
+#Drawing app
+
+Simple app for experimental drawing on a HTML5 Canvas.
+
+
+##Todo:
+* Functionality for saving a drawing
+* Being able to add images
+
---
+++ script.js
@@ -0,0 +1,45 @@
+//Load Canvas
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+//Set variables
+var radius = 10;
+var dragging = false;
+
+//Retrieve canvas dimensions
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+//Set Linewidth
+context.lineWidth = radius*2;
+
+var putPoint = function(e){
+	if(dragging){
+		context.lineTo(e.clientX, e.clientY);
+		context.stroke();
+		context.beginPath();
+		context.arc(e.clientX, e.clientY, 
+				radius, 0, Math.PI*2);
+		context.fill();
+		context.beginPath();
+		context.moveTo(e.clientX, e.clientY);	
+	}
+	
+}
+
+var engage = function(e){
+	dragging = true;
+	putPoint(e);
+}
+
+var disEngage = function(){
+	dragging = false;
+	context.beginPath();
+}
+
+
+
+canvas.addEventListener('mousedown', engage);
+canvas.addEventListener('mousemove', putPoint);
+canvas.addEventListener('mouseup', disEngage)
+
